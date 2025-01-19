bea2bfb38ead90ecfee8eb2ce7a5d41ec4229b67
Theo Sementa <theosementa@gmail.com>
2025-01-19 10:48:15 +0100

    Initial Commit

---
+++ POC_GithubChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC_GithubChart
+//
+//  Created by Theo Sementa on 19/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC_GithubChart/POC_GithubChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_GithubChartApp.swift
+//  POC_GithubChart
+//
+//  Created by Theo Sementa on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct POC_GithubChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
